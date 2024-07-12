10c7f7af5ccbbbf7a2dc1f9f722113e91febfd0b
Abdulbasit Imam <abdulbasitkayode@gmail.com>
2024-07-12 21:19:34 +0100

    Little change to make my codes more Readable

--- quiz.js
+++ quiz.js
@@ -1,22 +1,10 @@
-// function checkAnswer () {
-//     const correctAnswer = "4";
-//     const userAnswer = document.querySelector('name=quiz').value;
-//     let feedback = document.getElementById('feedback')
-
-//     if (userAnswer === correctAnswer) {
-//         feedback.textContent = "Correct! Well done."
-//  } else {
-//     feedback.textContent = "That's incorrect. Try again!"
-//  }
-// }
-
-// document.getElementById('submit-answer').addEventListener('click', checkAnswer);
 // A BASIC QUIZ APPLICATION
+
 function checkAnswer() {
     const correctAnswer = "4";
     const userAnswer = document.querySelector('input[name="quiz"]:checked').value;
 
-    // Compare the learner's answer with the correct answer
+    // Compare the user's answer with the correct answer
     if (userAnswer === correctAnswer) {
         document.getElementById('feedback').textContent = "Correct! Well done.";
     } else {
